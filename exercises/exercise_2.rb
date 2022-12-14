9e59afb63ad800a1565dbbf1eb617994b8351f61
RuoningJin <ronjin2013@hotmail.com>
2022-12-13 21:04:44 -0500

    exercise 3 completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,8 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-@store1 = Store.where(id: 1)
-@store2 = Store.where(id: 2)
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
 
 @store1.update(name: 'Ron')
 
