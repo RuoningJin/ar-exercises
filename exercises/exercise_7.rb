875cf7256edfa6737368223f5c696d7f9ed591aa
RuoningJin <ronjin2013@hotmail.com>
2022-12-14 01:39:17 -0500

    exercise 7 completed

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name"
+print '>'
+store_name = $stdin.gets.chomp
+
+@new_store = Store.create(name: "#{store_name}")
+@new_store.save
+
+@errors = @new_store.errors.each {|error| p error}
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_have_something_to_sell
+
+  def must_have_something_to_sell
+    if !mens_apparel && !womens_apparel
+      errors.add("mens_apparel, womens_apparel", "must have something to sell")
+    end
+  end
 end
